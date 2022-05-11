aef46eb8ab877cd97db39867e1d53619e0e35f8a
Gulam Gaush <mohammadgulamgaush@gmail.com>
2022-05-11 10:54:26 +0530

    sidebar skeleton

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './home.css'
 import Topbar from '../../components/topbar/Topbar'
+import Feed from '../../components/feed/Feed'
+import Rightbar from '../../components/rightbar/Rightbar'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <Topbar/>
+    <>
+      <Topbar/>
+      <div className="homeContainer">
+        <Sidebar/>
+        <Feed/>
+        <Rightbar/>
+      </div>
+      
+    </>
+    
   )
 }
 
